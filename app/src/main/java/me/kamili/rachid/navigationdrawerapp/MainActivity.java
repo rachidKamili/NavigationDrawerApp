b92c49dd7971e64ae55796428def3eee8fefa51b
rachidKamili <rachidkamili.pro@gmail.com>
2018-04-01 00:49:57 -0400

    Adding Navigation Drawer + ActionBar

--- app/src/main/java/me/kamili/rachid/navigationdrawerapp/MainActivity.java
+++ app/src/main/java/me/kamili/rachid/navigationdrawerapp/MainActivity.java
@@ -1,13 +1,44 @@
 package me.kamili.rachid.navigationdrawerapp;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initiateToolbarAndActionBar();
+
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
     }
+
+    private void initiateToolbarAndActionBar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
