3f922df5661b6b1866f1239327f2911b6f5ebdc4
rachidKamili <rachidkamili.pro@gmail.com>
2018-04-01 20:23:11 -0400

    Adding buttons on the music toolbar: play + pause + restart + stop

--- app/src/main/java/me/kamili/rachid/navigationdrawerapp/MusicActivity.java
+++ app/src/main/java/me/kamili/rachid/navigationdrawerapp/MusicActivity.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,20 +20,21 @@ import java.util.List;
 
 public class MusicActivity extends BaseActivity {
 
-    private ListView musicList ;
+    private ListView musicList;
     private MediaPlayer mMediaPlayer;
+    private int mLength = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-        super.onCreateDrawer(this,R.id.nav_music,R.string.music_title);
+        super.onCreateDrawer(this, R.id.nav_music, R.string.music_title);
 
         musicList = findViewById(R.id.music_list);
 
         //get all the ids+names form raw folder
         List<String> values = new ArrayList<>();
-        Field[] fields=R.raw.class.getFields();
+        Field[] fields = R.raw.class.getFields();
         for (Field field : fields) {
             //fields[count].getInt(fields[count])
             values.add((field.getName()).substring(0, 1).toUpperCase() + (field.getName()).substring(1));
@@ -63,16 +67,9 @@ public class MusicActivity extends BaseActivity {
                     e.printStackTrace();
                 }
 
-                // Show Alert
-//                Toast.makeText(activity,
-//                        "Position :"+position+"  ListItem : " +(String) musicList.getItemAtPosition(position) , Toast.LENGTH_LONG)
-//                        .show();
             }
         });
-
     }
-
-
 
     @Override
     protected void onPause() {
@@ -87,11 +84,65 @@ public class MusicActivity extends BaseActivity {
     }
 
     private void releaseMediaPlayer() {
-        if( mMediaPlayer != null ) {
+        if (mMediaPlayer != null) {
             mMediaPlayer.release();
             mMediaPlayer = null;
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.music_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.play_music:
+                playMusic();
+                return true;
+            case R.id.pause_music:
+                pauseMusic();
+                return true;
+            case R.id.restart_music:
+                restartMusic();
+                return true;
+            case R.id.stop_music:
+                stopMusic();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void stopMusic() {
+        releaseMediaPlayer();
+        int count = musicList.getChildCount();
+        for (int i = 0; i < count; i++) {
+            musicList.getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+        }
+    }
+
+    private void restartMusic() {
+        if(mMediaPlayer != null){
+            mMediaPlayer.seekTo(0);
+            mMediaPlayer.start();
+        }
+    }
+
+    private void pauseMusic() {
+        if(mMediaPlayer != null  && mMediaPlayer.isPlaying()){
+            mMediaPlayer.pause();
+            mLength = mMediaPlayer.getCurrentPosition();
+        }
+    }
+
+    private void playMusic() {
+        if(mMediaPlayer != null && !mMediaPlayer.isPlaying()){
+            mMediaPlayer.seekTo(mLength);
+            mMediaPlayer.start();
+        }
+    }
 }
